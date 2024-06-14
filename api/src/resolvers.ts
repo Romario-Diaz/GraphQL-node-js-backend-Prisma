266f339633c518ad2b3d3bb145266aa2bb042667
Romario-Diaz <domsunday.22@gmail.com>
2024-06-13 21:38:27 -0500

    feat: 🎸 adding schemas and resolvers

---
+++ api/src/resolvers.ts
@@ -0,0 +1,14 @@
+import * as avo from './modules/avocado/avocado.resolver'
+import * as scalars from './modules/base/scalars.model'
+
+export default {
+  ...scalars,
+  Query: {
+    avo: avo.findOne,
+    avos: avo.findAll,
+  },
+  Mutation: {
+    createAvo: avo.createAvo,
+  },
+  Avocado: avo.resolver
+}
